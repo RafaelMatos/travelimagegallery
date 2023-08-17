f026b30afbefa292fb0658a0b0ff102eef9ea255
RafaelMatos <rafael-matos@outlook.com>
2023-08-17 11:37:29 -0300

    Background Video - using switch case to correct slider on production

--- src/components/HomeSection/index.tsx
+++ src/components/HomeSection/index.tsx
@@ -5,23 +5,50 @@ import {
   Title,
   Video,
 } from "./styles";
-// import{ useContext } from 'react'
+import { useContext, useEffect, useState } from "react";
 import {
   TwitterLogo,
   InstagramLogo,
   FacebookLogo,
 } from "@phosphor-icons/react";
+import veniceMp4 from "./../../assets/venice/bg.mp4";
+import kotorMp4 from "./../../assets/kotor/bg.mp4";
 import mykonosMp4 from "./../../assets/mykonos/bg.mp4";
+import santoriniMp4 from "./../../assets/santorini/bg.mp4";
+import anconaMp4 from "./../../assets/ancona/bg.mp4";
 import { SliderNavigation } from "./components/SliderNavigation";
-// import { SliderContext } from "../../contexts/SliderContext";
+import { SliderContext } from "../../contexts/SliderContext";
 export function HomeSection() {
-  // const { localSelected,localSelectedVideoUrl } = useContext(SliderContext)
+  const { localSelected } = useContext(SliderContext);
+  const [videoSelected, setVideoSelected] = useState(mykonosMp4);
+  useEffect(() => {
+
+    switch (localSelected) {
+      case "mykonos":
+        setVideoSelected(mykonosMp4)
+        break;
+      case "venice":
+        setVideoSelected(veniceMp4)
+        break;
+      case "kotor":
+        setVideoSelected(kotorMp4)
+        break;
+      case "santorini":
+        setVideoSelected(santoriniMp4)
+        break;
+      case "ancona":
+        setVideoSelected(anconaMp4)
+        break;
+      case "none":
+        setVideoSelected(mykonosMp4)
+        break;
+    }
+  }, [localSelected]);
 
   return (
     <HomeSectionContainer>
-      
       {/* <Video src={localSelected === 'none' ?  mykonosMp4 : localSelectedVideoUrl } autoPlay muted loop /> */}
-      <Video src={ mykonosMp4 } autoPlay muted loop />
+      <Video src={videoSelected ? videoSelected : mykonosMp4} autoPlay muted loop />
       <ContentContainer>
         <Title>
           Wonderful.
@@ -46,7 +73,7 @@ export function HomeSection() {
           <FacebookLogo weight="fill" />
         </a>
       </SocialMediaContainer>
-      <SliderNavigation/>
+      <SliderNavigation />
     </HomeSectionContainer>
   );
 }
